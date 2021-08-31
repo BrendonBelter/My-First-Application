5c3db4192e32022b41de26af647b7cd2110ff533
BrendonBelter <89850068+BrendonBelter@users.noreply.github.com>
2021-08-31 15:12:46 +0100

    Updates

---
+++ My First Application/My First Application/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        class FirstClass
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("What is your name?");
+                Console.Write("Type your first name \n"); 
+                string myFirstName;
+                myFirstName = Console.ReadLine();
+
+                string myLastName;
+                Console.Write("Type your last name \n");
+                myLastName = Console.ReadLine();
+
+                Console.WriteLine("Hello," + myFirstName + "" + myLastName);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
